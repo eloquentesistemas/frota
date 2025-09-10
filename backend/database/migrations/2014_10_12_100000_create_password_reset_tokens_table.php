a88acaf8188492d80a29f7e00f4e0d03e28d7980
luanmarcosfgns <luanmarcosfgns@gmail.com>
2025-09-10 14:40:43 -0400

    FEITO AS CORREÇÕES PEDIDAS PELA CLIENTE PÁRA A IMPLANTAÇÃO

--- backend/database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
+++ backend/database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
@@ -11,11 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
+        try {
+
+            Schema::create('password_reset_tokens', function (Blueprint $table) {
+                $table->string('email')->primary();
+                $table->string('token');
+                $table->timestamp('created_at')->nullable();
+            });
+        }catch (\Exception $e){}
     }
 
     /**
